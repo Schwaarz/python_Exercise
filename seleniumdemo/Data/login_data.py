02004d55282b38632fe267ccb665a7ac1e1f6d57
YGH <2071419073@qq.com>
2021-08-04 23:05:24 +0800

    练习selenium

--- seleniumdemo/Data/login_data.py
+++ seleniumdemo/Data/login_data.py
@@ -1,3 +1,17 @@
+import xlrd
+from seleniumdemo.Business import search
+def login_data():
+    workbook = xlrd.open_workbook('C:\\Users\\yinguohao\\Downloads\\测试账号表.xlsx')
+    sheet = workbook.sheet_by_name("Sheet1")
+    rows = sheet.nrows
+    data1 = []
+    for i in range(1,rows):
+        line = sheet.row_values(i)
+        data1.append(line)
+    for a in range(len(data1)):
+        search.sinitekwh_login(data1[a][0], str(data1[a][1]))
+        # print(str(data1[a][0]))
 
-username = '1'
-password = '2'
+
+
+print(login_data())
