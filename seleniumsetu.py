2f99832641e3fc7eb652af2b2b8b373e1c5f66bd
YGH <2071419073@qq.com>
2022-10-16 22:35:30 +0800

    多任务爬虫&图片去重

--- seleniumsetu.py
+++ seleniumsetu.py
@@ -1,11 +1,13 @@
 import os
 import re
+import threading
+import time
 from urllib.error import HTTPError
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from bs4 import BeautifulSoup
-from selenium.webdriver.chrome.options import Options
 from urllib.request import urlretrieve
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 def se_tu():
@@ -23,7 +25,7 @@ def se_tu():
         link = img[0]['src']  # 提取img的链接
         try:
             urlretrieve(link, 'd:/pic/' + str(result) + '.jpg')
-            path = 'D:\ pic'
+            path = 'D:/pic'
             mkdir = os.listdir(path)  # 获取已爬取目录图片数量
             print('成功！爬取第' + str(len(mkdir)) + '张图片')  # 爬取计数
         except HTTPError:
@@ -32,4 +34,18 @@ def se_tu():
         continue
 
 
-se_tu()
+class MyThread(threading.Thread):
+    def run(self):
+        for i in range(5):
+            se_tu()
+            time.sleep(1)
+            print(self.name, i)
+
+
+t1 = MyThread()
+t2 = MyThread()
+t3 = MyThread()
+t1.start()
+t2.start()
+t3.start()
+print(threading.enumerate())
